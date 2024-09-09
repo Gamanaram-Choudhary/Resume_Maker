a45cc78f27d6fccbd638e944bb502aa4e33b54c4
Gamanaram_Choudhary <130850807+Gamanaram-Choudhary@users.noreply.github.com>
2024-09-09 19:05:46 +0530

    URL Updated

--- Client/src/components/Authentication/Login.js
+++ Client/src/components/Authentication/Login.js
@@ -18,7 +18,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:4000/login", {
+      const response = await fetch("https://resume-maker-rosy.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
