827cdf01bf7b7ac7ecf3683419398ca973dc1b8e
Gamanaram_Choudhary <130850807+Gamanaram-Choudhary@users.noreply.github.com>
2024-09-09 19:30:59 +0530

    Cors issue fix

--- Server/index.js
+++ Server/index.js
@@ -11,8 +11,13 @@ app.use(
   cors({
     origin: "https://resume-maker-fronted.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Range", "X-Content-Range"], 
+    // maxAge: 86400, 
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());   
 app.use("/", router);
