610d05511cd32b61732a44703743eda986717fcc
Gamanaram_Choudhary <130850807+Gamanaram-Choudhary@users.noreply.github.com>
2024-09-09 19:19:51 +0530

    URL Update

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ import router from "./routes/user.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://resume-maker-fronted.vercel.app/",
     credentials: true,
   })
 );
